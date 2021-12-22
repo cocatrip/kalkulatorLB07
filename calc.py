8f65dcf4eb4ec5d670960be3e48fd1f2b5f2ade9
globalpolaris <54710635+globalpolaris@users.noreply.github.com>
2021-12-22 15:53:51 +0700

    added perkalian

--- calc.py
+++ calc.py
@@ -4,10 +4,14 @@ def pengurangan(a, b):
     else:
         print(b - a)
 
+def perkalian(a,b):
+    return a*b
+
 def penambahan(a,b):
     return a+b
 
 def main():
     print(penambahan(5,1))
+    print(perkalian(5,1))
 
 main()
